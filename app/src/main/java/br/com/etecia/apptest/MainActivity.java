d3dcc44cb7f1bfc08c1cf4b61c2ea59f8b3d6d06
LuiSaad <luisa.lima2904@gmail.com>
2023-03-30 21:44:20 -0300

    barra coordinator with icons3

--- app/src/main/java/br/com/etecia/apptest/MainActivity.java
+++ app/src/main/java/br/com/etecia/apptest/MainActivity.java
@@ -3,6 +3,8 @@ package br.com.etecia.apptest;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 import com.google.android.material.appbar.MaterialToolbar;
 
@@ -16,6 +18,19 @@ public class MainActivity extends AppCompatActivity {
 
         //Apresentando o xml para o java
         idToolBar = findViewById(R.id.idToolBar);
+
+        //Criando click no menu de navegação
+        idToolBar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(),"Cliquei no menu hamburguer", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        //Criando clique nos itens de menu
+
+        idToolBar.setOnMenuItemClickListener(new);
+
 
     }
 }
