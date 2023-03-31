c67e72a0fd436e708837752329175836b2967a76
LuiSaad <luisa.lima2904@gmail.com>
2023-03-30 21:35:22 -0300

    barra coordinator with icons2

--- app/src/main/java/br/com/etecia/apptest/MainActivity.java
+++ app/src/main/java/br/com/etecia/apptest/MainActivity.java
@@ -4,11 +4,18 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.material.appbar.MaterialToolbar;
 
+public class MainActivity extends AppCompatActivity {
+    //Declrando uma variável global que irá representar o xml no Java
+    MaterialToolbar idToolBar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Apresentando o xml para o java
+        idToolBar = findViewById(R.id.idToolBar);
+
     }
 }
